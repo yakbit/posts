6edbcc1560b8cb082037a840eea21358b3636028
Oleh <oleh59@hotmail.com>
2023-03-17 14:28:01 +0100

    se crea la llamada a la API y obtiene datos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <div>Hola</div>;
-}
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts/")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  posts && console.log(posts);
+  return (
+    <div>
+      {posts &&
+        posts.map((post) => (
+          <div key={post.id}>
+            {post.userId}
+            <h1>{post.title}</h1>
+            <h3>{post.body}</h3>
+            <br />
+          </div>
+        ))}
+    </div>
+  );
+};
 
 export default App;
